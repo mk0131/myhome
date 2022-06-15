f1e60318e1acb6ae9e01ad3df7afafab6853b83e
mk0131 <jmg21128@gmail.com>
2022-06-15 22:13:46 +0900

    jpa 를 이용한 웹 페이지 처리

--- src/main/java/com/springboot/myrest/controller/BoardController.java
+++ src/main/java/com/springboot/myrest/controller/BoardController.java
@@ -4,6 +4,10 @@ import com.springboot.myrest.model.Board;
 import com.springboot.myrest.repository.BoardRepository;
 import com.springboot.myrest.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -21,8 +25,15 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
+        // Page<Board> boards = boardRepository.findAll(pageable);
+        // boards.getTotalElements(); // 총 개수 구하기
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4); // 최소 0페이지 부터 시작
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
         return "board/list";
     }
@@ -39,9 +50,9 @@ public class BoardController {
     }
 
     @PostMapping("/form")
-    public String formSubmit(@Valid Board board , BindingResult bindingResult) { // spring 이 not null, size 검사
-        boardValidator.validate(board,bindingResult); // content 가 null 인지 확인
-        if(bindingResult.hasErrors()){
+    public String formSubmit(@Valid Board board, BindingResult bindingResult) { // spring 이 not null, size 검사
+        boardValidator.validate(board, bindingResult); // content 가 null 인지 확인
+        if (bindingResult.hasErrors()) {
             return "board/form";
         }
         boardRepository.save(board); // id 의 키 값이 있으면 update 없으면 insert
